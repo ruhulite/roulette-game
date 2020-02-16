5d4cb836d3bda7c747e6eaf1e6dc5e2f822d8762
Md. Ruhul Amin Khan <corporate.ruhul@gmail.com>
2020-02-16 17:38:34 +0600

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/Bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="row">
+            <div className="col col-sm-12 col-md-12">
+              <h2>Header...</h2>
+            </div>
+          </div>
+        </div>
       </header>
+      <article className="App-header">
+        <div className="container">
+          <div className="row">
+            <div className="col col-sm-12 col-md-12">
+              <div>Content...</div>
+            </div>
+          </div>
+        </div>
+      </article>
+      <footer className="App-header">
+        <div className="container">
+          <div className="row">
+            <div className="col col-sm-12 col-md-12">
+              <div>Footer...</div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
